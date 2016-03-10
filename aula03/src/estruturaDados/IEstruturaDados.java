9661ea6b50100f5103d6494eb806e17ad0dda489
fabinhow12 <fabinhow1222@gmail.com>
2016-03-10 17:00:19 -0300

    Iniciando repositório

---
+++ aula03/src/estruturaDados/IEstruturaDados.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estruturaDados;
+
+/**
+ *
+ * @author fabio
+ */
+public interface IEstruturaDados {
+   
+    public void adicionaAluno(Aluno aluno);
+    
+    public void adicionaAluno(int posicao, Aluno aluno);
+    
+    public Aluno pegaAluno(int posicao);
+    
+    public void remove(int posicao);
+    
+    public boolean contem(Aluno aluno);
+    
+    public int tamanho();
+    
+    public String listaTodos();
+    
+    
+    
+}
